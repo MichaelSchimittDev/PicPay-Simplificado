46465dbdfca16f9b68dac7f3fe5b5e0174cb6af0
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-11-06 22:24:40 -0300

    Created TransactionRepository and UserRepository

---
+++ src/main/java/com/picpay/PicPaySimplificado/repositories/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.picpay.PicPaySimplificado.repositories;
+
+import com.picpay.PicPaySimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
+}
